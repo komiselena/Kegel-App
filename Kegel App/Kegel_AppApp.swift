fa0354cca8ff501e9c95fae1fb8bac9e3c81737a
komiselena <komiselen@yandex.ru>
2024-03-08 17:15:06 +0300

    first commit

---
+++ Kegel App/Kegel_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kegel_AppApp.swift
+//  Kegel App
+//
+//  Created by Mac on 08.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Kegel_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
